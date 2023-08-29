30c32914c5a520e130dad61f126f0e3bec908821
Sridhar <sridharonstar@gmail.com>
2023-08-29 13:10:34 +0530

    initial commit

---
+++ lib/Screens/expense.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: unused_field
+
+import 'package:flutter/material.dart';
+
+import '../models/expense_model.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: "American Tourist",
+        amount: 1999.0,
+        date: DateTime.now(),
+        category: Category.travel),
+        Expense(
+        title: "Flutter Course",
+        amount: 899.0,
+        date: DateTime.now(),
+        category: Category.work),
+        Expense(
+        title: "Italian pasta",
+        amount: 349.0,
+        date: DateTime.now(),
+        category: Category.food),
+        
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [Text("the Chart"), Text("Expense list...")],
+      ),
+    );
+  }
+}
