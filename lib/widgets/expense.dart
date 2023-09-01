7b54e3dd116970a49259d24612b718ae685eaa10
Sridhar <sridharonstar@gmail.com>
2023-09-01 15:54:38 +0530

    bottom showpop add button

--- lib/widgets/expense.dart
+++ lib/widgets/expense.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unused_field
 
+import 'package:expense_tracker/widgets/expenses_list/expense_newpop.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 
@@ -19,33 +20,39 @@ class _ExpensesState extends State<Expenses> {
         amount: 1999.0,
         date: DateTime.now(),
         category: Category.travel),
-        Expense(
+    Expense(
         title: "Flutter Course",
         amount: 899.0,
         date: DateTime.now(),
         category: Category.work),
-        Expense(
+    Expense(
         title: "Italian pasta",
         amount: 349.0,
         date: DateTime.now(),
         category: Category.food),
-        
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => NewExpense());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Expense App",),
-        actions:  [
-          IconButton(onPressed: () {}, icon: Icon(Icons.add))
-        ],
+        title: const Text(
+          "Expense App",
+        ),
+        actions: [IconButton(onPressed: () {_openAddExpenseOverlay();}, icon: const Icon(Icons.add))],
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [ const Text("the Chart"),
-           Expanded(child: ExpensesList(expenses: _registeredExpenses))],
-        ),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("the Chart"),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
+        ],
+      ),
     );
   }
 }
