9d028218939e716962f60eb820801d1fa1b29ff0
Sridhar <sridharonstar@gmail.com>
2023-09-02 21:10:30 +0530

    date time input add

--- lib/widgets/expenses_list/expense_newpop.dart
+++ lib/widgets/expenses_list/expense_newpop.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: unused_field
+import 'package:expense_tracker/models/expense_model.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -17,23 +18,34 @@ class _NewExpenseState extends State<NewExpense> {
   //or
   final _titleController = TextEditingController();
   final _amountControlller = TextEditingController();
+  DateTime? _selectedDate;
 
-void _presentDatePicker(){
-  final now = DateTime.now();
-  final firstDate = DateTime(now.year-1,now.month,now.day);
-  showDatePicker(context: context, initialDate: now, firstDate: firstDate, lastDate: now);
-}
-@override
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    // ignore: unused_local_variable
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  @override
   void dispose() {
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-           TextField(
+          TextField(
             controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(label: Text("Title")),
@@ -44,33 +56,48 @@ void _presentDatePicker(){
                 child: TextField(
                   controller: _amountControlller,
                   keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(label: Text("Amount"),prefixText: '\$'),
+                  decoration: const InputDecoration(
+                      label: Text("Amount"), prefixText: '\$'),
                 ),
               ),
-               const SizedBox(height: 16,),
-          Expanded(child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              const Text("Selected Date"),
-              IconButton(onPressed: () {_presentDatePicker();}, icon: Icon(Icons.calendar_month))
-            ],
-          )),
+              const SizedBox(
+                height: 16,
+              ),
+              Expanded(
+                  child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                   Text(_selectedDate == null ? "no date picked": formatter.format(_selectedDate!)),
+                  IconButton(
+                      onPressed: () {
+                        _presentDatePicker();
+                      },
+                      icon: const Icon(Icons.calendar_month))
+                ],
+              )),
             ],
           ),
-                        const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           Row(
             children: [
-              ElevatedButton(onPressed: () {
-                Navigator.pop(context);
-              }, child: const Text("Cancel")),  
-              const SizedBox(width: 20,),
               ElevatedButton(
                   onPressed: () {
-                    print(_titleController.text);
-                    print(_amountControlller.text);              
+                    Navigator.pop(context);
                   },
-                  child: const Text("Save Expense"),),
+                  child: const Text("Cancel")),
+              const SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountControlller.text);
+                },
+                child: const Text("Save Expense"),
+              ),
             ],
           )
         ],
