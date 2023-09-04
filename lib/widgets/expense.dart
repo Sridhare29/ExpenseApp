d162c25e7adbf1660b58d6ca3c646a8de575cc64
Sridhar <sridharonstar@gmail.com>
2023-09-04 12:34:06 +0530

    Expense delete with undo

--- lib/widgets/expense.dart
+++ lib/widgets/expense.dart
@@ -48,9 +48,20 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: const Text("Expense deleted."),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(label: "Undo", onPressed: (){
+        setState(() {
+          _registeredExpenses.insert(expenseIndex, expense);
+        });
+      }),
+      )
+    );
   }
 
   @override
